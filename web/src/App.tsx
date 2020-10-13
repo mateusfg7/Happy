7f0ee7ea430186b34bbbf119ac1a658a8550e412
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-13 13:54:42 -0300

    web: create router config

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
+import "./styles/global.css";
+
+import Routes from "./routes";
+
 function App() {
-  return (
-    <div className='App'>
-      <h1>Hello Word</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
