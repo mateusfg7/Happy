c984ce0e74b8c28197a251e5e030a109cae5e170
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 15:38:26 -0300

    backend: create orphanage view

---
+++ backend/src/views/orphanages_view.ts
@@ -0,0 +1,16 @@
+import Orphanage from "../models/Orphanage";
+
+export default {
+  render(orphanage: Orphanage) {
+    return {
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,
+    };
+  },
+};
