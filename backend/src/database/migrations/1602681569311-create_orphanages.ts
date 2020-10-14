f3e52679051d5a2d349db939be435187675a5b81
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 11:03:00 -0300

    backend: create opening_hours table

--- backend/src/database/migrations/1602681569311-create_orphanages.ts
+++ backend/src/database/migrations/1602681569311-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602681569311 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
