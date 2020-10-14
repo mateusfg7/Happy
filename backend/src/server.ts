b075e0b770116517a4165d66c6ed7847254fb000
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 16:09:59 -0300

    backend: use error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,13 @@ import "express-async-errors";
 import "./database/connection";
 
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 app.listen(3333);
