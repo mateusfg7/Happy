f43cd658fef8af9430f9754113fc0907919f8fb4
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 13:15:21 -0300

    backend: create index orphanages route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import OrphanageController from "./controllers/OrphanagesController";
 
 const routes = Router();
 
+routes.get("/orphanages", OrphanageController.index);
 routes.post("/orphanages", OrphanageController.create);
 
 export default routes;
