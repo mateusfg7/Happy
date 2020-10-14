4b5dae879ad636ee17dd59eb88a291d3b047867a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 14:12:26 -0300

    backend: add uploads in orphanages create route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "./config/upload";
 import OrphanageController from "./controllers/OrphanagesController";
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get("/orphanages", OrphanageController.index);
 routes.get("/orphanages/:id", OrphanageController.show);
-routes.post("/orphanages", OrphanageController.create);
+routes.post("/orphanages", upload.array("images"), OrphanageController.create);
 
 export default routes;
