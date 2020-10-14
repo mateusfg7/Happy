57b6f86d9f7d22627445aa49178e586fae2a490b
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 15:53:33 -0300

    backend: use image view on orphanage view

--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -1,4 +1,5 @@
 import Orphanage from "../models/Orphanage";
+import imagesView from "./images_view";
 
 export default {
   render(orphanage: Orphanage) {
@@ -11,6 +12,7 @@ export default {
       instructions: orphanage.instructions,
       opening_hours: orphanage.opening_hours,
       open_on_weekends: orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images),
     };
   },
 
