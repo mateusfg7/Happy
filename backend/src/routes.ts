e6157b6cc4e67f7ea0eee65d7e778bbd25722062
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 13:19:13 -0300

    backend: create show orphanage route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanageController from "./controllers/OrphanagesController";
 const routes = Router();
 
 routes.get("/orphanages", OrphanageController.index);
+routes.get("/orphanages/:id", OrphanageController.show);
 routes.post("/orphanages", OrphanageController.create);
 
 export default routes;
