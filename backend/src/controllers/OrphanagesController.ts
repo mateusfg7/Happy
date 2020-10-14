e21a33f7e6f3abc15c592224f348849a022ea872
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-14 15:58:24 -0300

    backend: use views on orphanages controller

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+
+import orphanageView from "../views/orphanages_view";
+
 import Orphanages from "../models/Orphanage";
 
 export default {
@@ -10,7 +13,7 @@ export default {
       relations: ["images"],
     });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request: Request, response: Response) {
@@ -22,7 +25,7 @@ export default {
       relations: ["images"],
     });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
