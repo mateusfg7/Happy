ca8642c280cc1f9ebe6084209f49fb5e31adb550
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-15 14:41:34 -0300

    web: make the image switcher

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -19,6 +19,7 @@ interface Orphanage {
   opening_hours: string;
   open_on_weekends: boolean;
   images: Array<{
+    id: number;
     url: string
   }>;
 }
@@ -30,6 +31,7 @@ interface OrphanageParams {
 export default function Orphanage() {
   const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0)
 
   useEffect(() => {
     api.get(`orphanages/${params.id}`).then((response) => {
@@ -48,47 +50,30 @@ export default function Orphanage() {
       <main>
         <div className='orphanage-details'>
           <img
-            src={orphanage.images[0].url}
+            src={orphanage.images[activeImageIndex].url}
             alt={orphanage.name}
           />
 
           <div className='images'>
-            <button className='active' type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
-            <button type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
-            <button type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
-            <button type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
-            <button type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
-            <button type='button'>
-              <img
-                src='https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg'
-                alt='Lar das meninas'
-              />
-            </button>
+            {
+              orphanage.images.map((image, index) => {
+                return (
+                  <button 
+                    key={image.id}
+                    className={activeImageIndex === index ? 'active' : ''}
+                    type='button'
+                    onClick={() => {
+                      setActiveImageIndex(index)
+                    }}
+                  >
+                    <img
+                      src={image.url}
+                      alt={orphanage.name}
+                    />
+                  </button>
+                )
+              })
+            }
           </div>
 
           <div className='orphanage-details-content'>
@@ -119,7 +104,7 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href=''>Ver rotas no Google Maps</a>
+                <a target="_blank" rel="noopener noreferrer" href={`https://google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
