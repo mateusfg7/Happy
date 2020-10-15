be8d3fe287dc0f00818e96b17613612dbd367612
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-15 09:27:44 -0300

    web: fix bug of routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
 import OrphanagesMap from "./pages/OrphanagesMap";
-import CreateOrphanage from "./pages/OrphanagesMap";
-import Orphanage from "./pages/OrphanagesMap";
+import CreateOrphanage from "./pages/CreateOrphanage";
+import Orphanage from "./pages/Orphanage";
 
 function Routes() {
   return (
