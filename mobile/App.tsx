3995fac00e7c611c7c858612ff158e2c5312fa43
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-16 09:56:25 -0300

    mobile: add the map and create a static marker

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -19.9026615,
+          longitude: -44.104138,
+          latitudeDelta: 1.0,
+          longitudeDelta: 1.0,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -19.9026615,
+            longitude: -44.104138,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Little Blockchain</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -14,8 +42,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 16,
     justifyContent: 'center',
+  },
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
