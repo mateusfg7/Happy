5094b47d225ab50a4bf98b25eb722ec44767665a
mateusfg7 <mateusfelipefg77@gmail.com>
2020-10-16 14:27:24 -0300

    mobile: use header component

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -11,6 +11,8 @@ import OrphanageDetails from './pages/OrphanageDetails';
 import SelectMapPosition from './pages/CreateOrphanages/SelectMapPosition';
 import OrphanageData from './pages/CreateOrphanages/OrphanageData';
 
+import Header from './components/Header';
+
 export default function Routes() {
   return (
     <NavigationContainer>
@@ -21,9 +23,33 @@ export default function Routes() {
         }}
       >
         <Screen name='OrphanagesMap' component={OrphanagesMap} />
-        <Screen name='OrphanageDetails' component={OrphanageDetails} />
-        <Screen name='SelectMapPosition' component={SelectMapPosition} />
-        <Screen name='OrphanageData' component={OrphanageData} />
+
+        <Screen
+          name='OrphanageDetails'
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title='Orfanato' />,
+          }}
+        />
+
+        <Screen
+          name='SelectMapPosition'
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title='Selecione no mapa' />,
+          }}
+        />
+
+        <Screen
+          name='OrphanageData'
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title='Informe os dados' />,
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
